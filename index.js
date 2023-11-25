e579f44915e27f1445962beed626265f74396920
Horidas Roy <royhoridas64@gmail.com>
2023-11-25 17:42:57 +0600

    user related api added successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const cors=require('cors')
+require('dotenv').config()
 const app=express()
 const port=process.env.PORT || 5000 ;
 
@@ -9,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.wcearye.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,16 +29,61 @@ async function run() {
 
     const userCollection = client.db('opiniunDB').collection('users')
     
+    //user related api
+    app.get('/users',async(req,res)=>{
+      const result= await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/users/admin/:email',async(req,res)=>{
+        const email=req.params.email;
+        //todo:verify user by using token
+        const query={email:email}
+        const user=await userCollection.findOne(query)
+        let admin=false;
+        if(user){
+          admin=user?.role="admin"
+        }
+        res.send({admin})
+    })
+
+    app.post('/users',async(req,res)=>{
+       const user=req.body;
+       const query={email:user?.email}
+       const existingUser= await userCollection.findOne(query)
+       if(existingUser){
+          return res.send({message:'User already exist',insertedId:null})
+       }
+       const result=await userCollection.insertOne(user)
+       res.send(result)
+    })
+    
+    app.patch('/users/admin/:id',async(req,res)=>{
+       const id=req.params.id;
+       const filter={_id : new ObjectId(id)}
+       const updatedDoc={
+          $set:{
+            role:"admin"
+          }
+       }
+       const result=await userCollection.updateOne(filter,updatedDoc)
+       res.send(result)
+    })
     
 
-
+    app.delete('/users',async(req,res)=>{
+       const id=req.params.id;
+       const query={_id : new ObjectId(id)}
+       const result = await userCollection.deleteOne(query)
+       res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
