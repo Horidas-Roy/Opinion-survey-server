a48f0a73c6db2a365c74f46c2cd707b397496d34
Horidas Roy <royhoridas64@gmail.com>
2023-11-28 00:24:34 +0600

    survey creation post api added successfully

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { ObjectId } = require('bson');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.wcearye.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -182,6 +183,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/surveys',async(req,res)=>{
+       const newSurvey=req.body;
+       const result=await surveyCollection.insertOne(newSurvey)
+       res.send(result)
+    })
+
     // vote related api
    app.get('/voters',async(req,res)=>{
        const result=await voterCollection.find().toArray();
@@ -254,6 +261,33 @@ async function run() {
         }
         const result=await surveyCollection.updateOne(filter,updateDoc)
         res.send(result)
+    })
+
+    //comments related api
+    app.put('/surveys/comment/:id',async(req,res)=>{
+       const id=req.params.id;
+       const comment=req.body;
+       const filter={_id:new ObjectId(id)}
+       const updateDoc={
+         $push:{
+          comments:comment
+         }
+       }
+       const result = await surveyCollection.updateOne(filter,updateDoc)
+       res.send(result)
+    })
+    // report related api
+    app.put('/surveys/report/:id',async(req,res)=>{
+      const id=req.params.id;
+      const report=req.body;
+      const filter={_id: new ObjectId(id)}
+      const updateDoc={
+        $push:{
+          reports:report
+        }
+      }
+      const result=await surveyCollection.updateOne(filter,updateDoc)
+      res.send(result)
     })
 
 
